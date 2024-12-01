0c370da885e9d4379aaaba97530b555891fee4ab
Bartek Skoniecki <b.skoniecki@interia.pl>
2024-12-01 19:24:11 +0100

    laby 5 zejsciowka

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+std::vector< char > foo(std::list< Human >& humans)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result;
+
+    for (auto& human : humans) {
+        human.birthday();
+
+
+        if (human.isMonster()) {
+            result.push_back('n');
+        }
+        else {
+            result.push_back('y'); 
+        }
+    }
+
+    // Odwracamy kolejnoœæ elementów w wektorze
+    std::reverse(result.begin(), result.end());
+
+    return result;
 }
